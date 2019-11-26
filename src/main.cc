21784b6ee3d1ed72a642bbff71930e109858ef8f
julietraevskaua <3Kinder333@gmail.com>
2019-11-26 17:40:34 +0200

    olala

---
+++ src/main.cc
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+#include <string>
+
+
+using namespace std;
+
+int main()
+{
+    string text;
+    int i = 0;
+    int count = 0;
+    getline(cin, text);
+    char const* s = text.c_str();
+    if (s[i] != '\0')  
+    {
+         while(s[i] != '\0')
+         {
+             if(s[i] == ' ')
+            {
+                count++;
+                i++;
+            }
+            else
+                i++;    
+        }
+        count++;
+    }
+    cout << count << endl;
+    return(0);    
+}
